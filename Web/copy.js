2a6418d9f88dc895e045fdc005826711d264f4b3
zImSkillz <83404249+zImSkillz@users.noreply.github.com>
2022-12-18 03:02:55 +0100

    Add files via upload

---
+++ Web/copy.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  window.addEventListener('message', function(event) {
+      var node = document.createElement('textarea');
+      var selection = document.getSelection();
+      node.textContent = event.data.copyThis;
+      document.body.appendChild(node);
+      selection.removeAllRanges();
+      node.select();
+      document.execCommand('copy');
+      selection.removeAllRanges();
+      document.body.removeChild(node);
+  });
+});
---
+++ fxmanifest.lua
@@ -0,0 +1,30 @@
+--[[
+
+				██╗  ██╗   ██╗██╗  ██╗ ██████╗ ███████╗
+				██║  ╚██╗ ██╔╝╚██╗██╔╝██╔═══██╗██╔════╝
+				██║   ╚████╔╝  ╚███╔╝ ██║   ██║███████╗
+				██║    ╚██╔╝   ██╔██╗ ██║   ██║╚════██║
+				███████╗██║   ██╔╝ ██╗╚██████╔╝███████║
+				╚══════╝╚═╝   ╚═╝  ╚═╝ ╚═════╝ ╚══════╝
+					Developer: zImSkillz#0001
+						> Let's help some Developer
+]]--
+
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'zImSkillz'
+
+client_scripts {
+	'Client/*.lua'
+}
+
+files {
+	'Web/index.html',
+	'Web/jquery.js',
+	'Web/copy.js'
+}
+
+ui_page 'Web/index.html'
+
+lua54 'yes'
